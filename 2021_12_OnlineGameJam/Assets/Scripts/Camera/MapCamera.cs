7e9f57db0982ee163f2b5f19ad25479fbc8d9a09
rrrmaster <dudgus9101@gmail.com>
2021-12-18 03:01:08 +0900

    no message

---
+++ 2021_12_OnlineGameJam/Assets/Scripts/Camera/MapCamera.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCamera : MonoBehaviour
+{
+    public Camera camera;
+    public Transform target;
+    public float y = 28.8f;
+
+
+    private void Update()
+    {
+        camera.transform.position = new Vector3(0, Mathf.FloorToInt((target.position.y + y * 0.5f)/ y)*y, -10);
+    }
+}
