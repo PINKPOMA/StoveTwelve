254f56dbadc37aac2ec79305c787b322f01f461b
최유빈 <choi82354@gmail.com>
2021-12-19 00:25:57 +0900

    add Ending End

---
+++ 2021_12_OnlineGameJam/Assets/YB_Script/EndingEnd.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingEnd : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("신 전환 넣기");
+    }
+}
